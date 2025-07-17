adc6d459f66889d99fed9a7dc3d070949998e36e
meghanasvd30@gmail.com <meghanasvd30@gmail.com>
2025-07-17 11:51:02 +0000

    latest1

--- resource.tf
+++ resource.tf
@@ -2,3 +2,17 @@ resource "azurerm_resource_group" "terrakota" {
     name = "terrakota" 
     location = "East US" 
     }
+
+resource "azurerm_storage_account" "mysaterra09" {
+    name                     = "storagehk09"
+    resource_group_name      = azurerm_resource_group.terrakota.name
+    location                 = azurerm_resource_group.terrakota.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
+ 
+resource "azurerm_storage_container" "mysacontainer10" {
+    name                  = "my-casestudy-container1"
+    storage_account_name  = azurerm_storage_account.terrakota.name
+    container_access_type = "private"
+}
